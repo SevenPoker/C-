8527b347d1f2d5e07de076d17692f2b464313bbb
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 10:38:01 +0900

    Update 4_알고리즘과함수2.cpp

--- DAY2/4_알고리즘과함수2.cpp
+++ DAY2/4_알고리즘과함수2.cpp
@@ -1,8 +1,13 @@
-﻿#include <iostream>
+#include <iostream>
 #include <list>
 #include <algorithm>
 #include "show.h"
 
+
+int foo(int a, int b)
+{
+    return a + b;
+}
 
 int main()
 {
@@ -11,26 +16,16 @@ int main()
     std::list<int> s3;
 
     // transform 알고리즘
+    std::transform(s1.begin(), s1.end(), // 1. 이 구간의 내용을
+                   s2.begin(),           // 2. 이 구간의 내용과 함께
+//                   s3.begin(),           // 4. 함수 결과를 이곳에 넣어라
+                   std::back_inserter(s3),
+                   foo);                 // 3. 이 함수에 보내서
+
+    // 주석을 보고, foo 가 단항일까 이항일까 생각
+
+    show(s3);
+
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
