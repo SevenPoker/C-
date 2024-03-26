79873bef87012fd09ff8c43853aca2723a7f5987
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 11:38:46 +0900

    Update 5_함수객체4.cpp

--- DAY2/5_함수객체4.cpp
+++ DAY2/5_함수객체4.cpp
@@ -1,14 +1,26 @@
-﻿// 3_함수객체4 -  137 page 제일 윗부분내용
+// 3_함수객체4 -  137 page 제일 윗부분내용
        int Add1(int a, int b) { return a + b; }
 inline int Add2(int a, int b) { return a + b; }
 
 int main()
 {
 	int n1 = Add1(1, 2); 
+	// 함수 호출이다.
+	// 1. 인자인 1,2 를 약속된 장소 (레지스터)에 넣고
+	// 2. Add1 함수 위치로 이동 (call Add1)
+
 	int n2 = Add2(1, 2); 
+	// Add2의 기계어 코드를 이 위치에 치환해달라
+	// 단점인 경우는 코드 사이즈가 크면 파일 size가 엄청나게 커진다.
+	// mov eax, 2
+	// add eax, 1
+	// mov n2, eax
+
 
 	int(*f)(int, int) = &Add2;
 
 	f(1, 2); // ?
 }
 
+// godbolt.org 사이트 접속
+// => 다양한 언어의 컴파일 결과를 기계어(어셈블리)로 볼 수 있는 사이트
