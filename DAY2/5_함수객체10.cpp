8efb7914873f08f84c602662bc414955a105442e
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 14:38:34 +0900

    Update 5_함수객체10.cpp

--- DAY2/5_함수객체10.cpp
+++ DAY2/5_함수객체10.cpp
@@ -2,26 +2,35 @@
 #include <vector>
 #include <algorithm>
 
-bool foo(int n) { return n % 3 == 0; }
-
 int main()
 {
     std::vector<int> v = {1,2,6,4,3,5,7,9, 8,10};
 	
-	// 주어진 구간에서 1번째 나오는 "3의 배수" 을 찾고 싶다.
-    auto ret1 = std::find_if(v.begin(), v.end(), foo);
-
-	int k = 3; // 사용자가 입력한 값이라고 가정.
-
+	int k = 3;
 
 	// 주어진 구간에서 1번째 나오는 "k의 배수" 을 찾고 싶다.
-    // 이항으로 만들고 싶지만 foo는 단항이어야 한다.
 
-    auto ret2 = std::find_if(v.begin(), v.end(), foo);
+    // 람다 표현식은 "지역변수를 캡쳐 " 할 수 있다.
+	// [k] : 람다표현식에서 k를 사용하겠다는 의미
+	auto ret1 = std::find_if(v.begin(), v.end(),
+					[k](int n){return n % k == 0;});
 
-    // ==> 일반 함수인 foo에서는 main의 지역변수 k를 사용할 수 없다.
-    // ==> 결국, 전역변수가 도입되어야한다.
+	// ----------------------------------
+	// compiler가 아래처럼 코드를 만들어 준다.
 
+	class CompilerGeneratedName
+	{
+		int k; // 람다 표현식에서 갭쳐를 사용하면 멤버 데이터가 추가가 된다.
+	public:
+		CompilerGeneratedName(int n) : k(n){}
+		inline auto operator()(int n) const
+		{
+			return n % k == 0;
+		} 
+	};
+
+	auto ret1 = std::find_if(v.begin(), v.end(),CompilerGeneratedName(k));
+	// 결국 함수객체3.cpp 에서 만든 "IsDivide" 와 동일한 원리이다.
 }
 
 
