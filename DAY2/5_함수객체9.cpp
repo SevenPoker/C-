cfa1d5047d11975f0bb0d0cfe917242f1371b69a
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 14:06:27 +0900

    Add files via upload

---
+++ DAY2/5_함수객체9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+bool cmp(int a, int b) { return a < b; }
+
+int main()
+{
+	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
+
+	std::sort(v.begin(), v.end(), [](int a, int b){ return a < b;});
+}
+
+
+
+
+
+
+
+
+
