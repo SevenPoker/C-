2b5114ca7a710481f6a31394af1ae1016ff0f0f6
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 16:47:42 +0900

    Add files via upload

---
+++ DAY2/10_선형컨테이너3-1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include "show.h"
+
+int main()
+{
+	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
+
+	auto p = std::remove(v.begin(), v.end(), 3);
+
+	std::cout << v.size() << ", " << v.capacity() << std::endl;
+
+	v.erase(p,v.end());
+
+	return 0;
+}
+
