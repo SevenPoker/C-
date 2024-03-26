b665a9601e62bc14ae88c64cb30a286f0bad1043
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 14:11:06 +0900

    Update 5_함수객체9.cpp

--- DAY2/5_함수객체9.cpp
+++ DAY2/5_함수객체9.cpp
@@ -9,6 +9,20 @@ int main()
 	std::vector<int> v = { 1,3,5,7,9,2,4,6,8,10 };
 
 	std::sort(v.begin(), v.end(), [](int a, int b){ return a < b;});
+
+	// -------------------------------------
+	// 람다표현식의 원리
+	// => 위 코드를 컴파일러가 아래 코드처럼 변경해준다.
+
+	class CompilerGeneratedName
+	{
+	public:
+		inline auto operator()(int a, int b)
+		{
+			return a < b;
+		}
+	}
+	std::sort(v.begin(), v.end(), CompilerGeneratedName());
 }
 
 
