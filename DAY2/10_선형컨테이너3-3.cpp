cabfdb97bf8de1406f2f73c73c3a1499d848dee3
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 17:01:52 +0900

    Update 10_선형컨테이너3-3.cpp

--- DAY2/10_선형컨테이너3-3.cpp
+++ DAY2/10_선형컨테이너3-3.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <list>
 #include <algorithm>
@@ -6,11 +6,14 @@
 
 int main()
 {
-	std::vector<int> v1;
+	std::vector<int, debug_alloc<int> > v1;
 
+	v1.reserve(100);
+	// 아래 코드가 몇번의 할당이 있을것인가?
+	// 2배씩 커진다.
 	for (int i = 0; i < 30; i++)
 	{
-		
+		v1.push_back(0);
 	}
 }
 
