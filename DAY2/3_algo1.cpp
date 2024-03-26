0198407513f3bb1179110e804f645eed4b29117c
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 09:58:55 +0900

    Update 3_algo1.cpp

--- DAY2/3_algo1.cpp
+++ DAY2/3_algo1.cpp
@@ -1,9 +1,17 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <list>
 #include <algorithm>
 
 // 알고리즘 기본
+// 일반적인 의미 : 문제를 해결하는 방법ㄴ
+
+// STL에서는 std::find와 같은 일반 함수 템플릿
+
+// <algorithm>, <numeric>
+
+// 핵심 : 멤버 함수가 아닌 일반함수(템플릿)
+
 int main()
 {
 	std::vector<int> v = { 1,2,3,1,2,3,1,2,3,1 };
@@ -13,22 +21,3 @@ int main()
 	return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
