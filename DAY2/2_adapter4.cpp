223ccfe8a9bc712315f7e4002ffe3190e40ad783
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 09:46:23 +0900

    Update 2_adapter4.cpp

--- DAY2/2_adapter4.cpp
+++ DAY2/2_adapter4.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <list>
 #include <algorithm>
 
@@ -6,5 +6,22 @@ int main()
 {
 	std::list<int> s = { 1,2,3,4,5,6,7,3,9,10 };
 
-	std::list<int>::iterator p1 = s.begin(); 
+	// container에서 꺼낼수 있는 반복자는 4가지 종류가 있다.
+	// #1. 정방향 반복자
+	std::list<int>::iterator p1 = s.begin(); // 일반함수버전 : std::begin(s)
+
+	// #2. 역방향 반복자
+	std::list<int>::reverse_iterator p2 = s.rbegin(); // std::rbegin(s)
+
+	// #3. 상수반복자
+	// 아래는 쓰기가 안된다.
+	std::list<int>::const_iterator p3 = s.cbegin(); // std::cbegin(s)
+	// *p3 = 10; // error 쓰기 안됨
+	int n = *p3;
+
+	// #4. 상수 역방향 반복자
+	std::list<int>::const_reverse_iterator p4 = s.crbegin(); // std::crbegin(s)
+
+
+
 }
