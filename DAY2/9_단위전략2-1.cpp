24c57321e84a8657d73e530a4b3b146bfc417bd7
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 15:57:15 +0900

    Add files via upload

---
+++ DAY2/9_단위전략2-1.cpp
@@ -0,0 +1,26 @@
+﻿#include <vector>
+
+// 기본적으로는 std의 기본 allocator를 쓴다.
+// new와 delete로 이루어져 있다.
+template<typename T, typename Ax = std::allocator<T>> 
+class vector
+{
+	Ax ax; // 메모리 할당기 (allocator)
+		   // vector의 모든 멤버 함수에서 메모리 할당이 필요하면
+		   // ax 객체의 멤버함수를 사용해야 한다.
+public:
+	void resize(int sz)
+	{
+		T* p = ax.allocate(sz); // 할당
+
+		ax.deallocate(p, sz);   // 해지
+	}
+};
+
+int main()
+{
+	std::vector<int> v(5);
+	v.resize(10);
+}
+
+
