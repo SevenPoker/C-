0b42c667130269ba83fbefff6acd5510cd9d12d0
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-26 11:05:54 +0900

    Update 5_함수객체2.cpp

--- DAY2/5_함수객체2.cpp
+++ DAY2/5_함수객체2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 
@@ -11,8 +11,16 @@ int main()
 	// 주어진 구간에서 1번째 나오는 "3의 배수" 을 찾고 싶다.
     auto ret1 = std::find_if(v.begin(), v.end(), foo);
 
-
 	int k = 3; // 사용자가 입력한 값이라고 가정.
+
+
+	// 주어진 구간에서 1번째 나오는 "k의 배수" 을 찾고 싶다.
+    // 이항으로 만들고 싶지만 foo는 단항이어야 한다.
+
+    auto ret2 = std::find_if(v.begin(), v.end(), foo);
+
+    // ==> 일반 함수인 foo에서는 main의 지역변수 k를 사용할 수 없다.
+    // ==> 결국, 전역변수가 도입되어야한다.
 
 }
 
