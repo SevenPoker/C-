3a8a54eba3ec3dea60e21fb31ac776c9f223acc2
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 16:53:51 +0900

    Update 10_사용자정의타입3.cpp

--- DAY3/10_사용자정의타입3.cpp
+++ DAY3/10_사용자정의타입3.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #include <vector>
 #include <algorithm>
 #include "show.h"
@@ -31,8 +31,18 @@ int main()
 	v.emplace_back(3, 2);
 	v.emplace_back(0, 1);
 
-	std::sort(std::begin(v), std::end(v));
+	// 사용자 정의 타입을 컨테이너 보관할 수 있지만
+	// 알고리즘 사용시에는 알고리즘의 요구조건을 충족해야한다.
+	// => std::sort() 사용하기 위해서는 크기비교를 할 수 있어야한다.
 
+	// std::sort(v.begin(), v.end()); //error
+
+	// #1. 람다표현식으로 비교정책 전ㄴ달.
+	std::sort(v.begin(), v.end(), 
+				[](const Point& p1, const Point& p2){return p1.x < p2.x;});
+
+	// #2. Point 타입 자체의 크기 비교가 가능하면 된다.
+	
 }
 
 
