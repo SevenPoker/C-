e3f12edaa0df6f1a643b63d9fcd5e21a6a36eed8
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 09:55:49 +0900

    Add files via upload

---
+++ DAY3/3_array1-1.cpp
@@ -0,0 +1,28 @@
+﻿#include <vector>
+#include "show.h"
+
+void f1()
+{
+	int x[10];
+}
+
+void f2()
+{
+	std::vector<int> v(10);
+}
+
+void use_array()
+{
+	for(int i = 0; i < 1'000'000; i++) f1();
+}
+
+void use_vector()
+{
+	for(int i = 0; i < 1'000'000; i++) f2();
+}
+
+int main()
+{
+	chronometry(use_array);
+	chronometry(use_vector);
+}
