b7ab3a423b54357eeb9815c000ba449fac782495
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 11:06:46 +0900

    Update 4_컨테이너어답터4.cpp

--- DAY3/4_컨테이너어답터4.cpp
+++ DAY3/4_컨테이너어답터4.cpp
@@ -1,4 +1,4 @@
-﻿#include <stack>
+#include <stack>
 #include <queue> // queue, priority_queue
 #include <iostream>
 #include "show.h"
@@ -14,9 +14,21 @@ int main()
 	pq.push(20);
 	pq.push(15);
 
-	std::cout << pq.top() << std::endl; 
+	std::cout << pq.top() << std::endl; // 20
 	pq.pop();
-	std::cout << pq.top() << std::endl; 
+	std::cout << pq.top() << std::endl; // 15
+
+//	우선순위큐는 2개의 단위전략 사용
+//  내부 컨테이너, 비교 정책을 담은 함수 객체
+//  greater 쓸려면 중간에 container를 적어서 해야된다.
+//  cppreference가서 보면 된다.
+
+	// 단위 전략클래스는 요구조건이 있다.
+	// PQ 의 2번째 인자 : seqeunce container
+	// PQ 의 3번째 인자 : () 연산자를 재정의한 이항 함수 객체
+
+	std::priority_queue<int, std::vector<int>, std::greater<int> > pq2;
+
 
 }
 
