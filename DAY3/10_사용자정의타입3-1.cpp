9d1aad33586e3ec3bfced6caea498536497c10db
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 17:02:51 +0900

    Update 10_사용자정의타입3-1.cpp

--- DAY3/10_사용자정의타입3-1.cpp
+++ DAY3/10_사용자정의타입3-1.cpp
@@ -1,7 +1,11 @@
-﻿
+
 #include <vector>
 #include <algorithm>
 #include "show.h"
+
+using namespace std::rel_ops; // !=, <=, >=, > 의 template 버전 제공
+
+
 class Point
 {
 public:
@@ -21,9 +25,25 @@ public:
 	{
 		std::cout << x << ", " << y << std::endl;
 	}
+	// 사용자 정의 타입의 크기 비교를 위해서는 < 와 == 를 만드세요.
+	// 이 두개를 정의하면 알아서 되는 것이 있다.
+	bool operator<(const Point& other) const
+	{
+		return x < other.x;
+	}
+	bool operator==(const Point& other) const
+	{
+		return x == other.x && y == other.y;
+	}
 };
+
 int main()
 {
+	Point p1(1,2);
+	Point p2(3,4);
+
+	bool b = p1 > p2; // ok
+
 	std::vector<Point> v;
 
 	v.emplace_back(1, 2); // Point(1,2)
@@ -31,18 +51,7 @@ int main()
 	v.emplace_back(3, 2);
 	v.emplace_back(0, 1);
 
-	// 사용자 정의 타입을 컨테이너 보관할 수 있지만
-	// 알고리즘 사용시에는 알고리즘의 요구조건을 충족해야한다.
-	// => std::sort() 사용하기 위해서는 크기비교를 할 수 있어야한다.
-
-	// std::sort(v.begin(), v.end()); //error
-
-	// #1. 람다표현식으로 비교정책 전ㄴ달.
-	std::sort(v.begin(), v.end(), 
-				[](const Point& p1, const Point& p2){return p1.x < p2.x;});
-
-	// #2. Point 타입 자체의 크기 비교가 가능하면 된다.
-	
+	std::sort(v.begin(),v.end());
 }
 
 
