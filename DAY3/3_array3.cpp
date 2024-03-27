15f36187b8a134ca57834b42f0fe29d0348d6b4c
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 10:06:50 +0900

    Update 3_array3.cpp

--- DAY3/3_array3.cpp
+++ DAY3/3_array3.cpp
@@ -7,7 +7,9 @@ int main()
 
 	std::cout << arr.size() << std::endl;
 
-	arr.push_back(0);
-	arr.resize(20);
+	// std::array 는 결국 "raw array" 이다.
+	// "raw array 로 할 수 없는 작업"은 std::array로도 불가능합니다.
+	arr.push_back(0); // error 진짜 배열이 들어있기 때문에 사이즈를 키울수 있는 방법은 없다.
+	arr.resize(20); // 이것도 마찬가지로 안된다.
 
 }
