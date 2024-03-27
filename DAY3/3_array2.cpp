d639833f5d7b232fec319c7b93c4f7a44bc0447b
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 10:03:14 +0900

    Update 3_array2.cpp

--- DAY3/3_array2.cpp
+++ DAY3/3_array2.cpp
@@ -2,14 +2,25 @@
 #include <algorithm>
 #include "show.h"
 
+template<typename T, std::size_t N>
 struct array
 {
-	int buff[5];
+	T buff[N]; // stack에 저장된다.
+	int size() const { return N;}
 
-	int size() const { return 5;}
+	// STL의 규칙을 지켜야 한다.
+	using value_type = T;
+	using iterator = T*;
+
+	iterator begin() { return buff;}
+	iterator end() { return buff + N;}
 };
 
 int main()
 {
-	array arr = {1, 2, 3, 4, 5};
+	// 구조체 안에 배열이 있으면 아래처럼 초기화 가능합니다.
+	array<int, 5> arr = {1, 2, 3, 4, 5};
+
+	// raw array 는 멤버함수가 없지만 위에서 만든 array는 멤버 함수가 있습니다.
+	std::cout << arr.size() << std::endl;
 }
