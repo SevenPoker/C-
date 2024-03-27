907d1960504557a61e65ada3f9b04c0d517dacc3
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 12:06:32 +0900

    Update 6_set1.cpp

--- DAY3/6_set1.cpp
+++ DAY3/6_set1.cpp
@@ -1,7 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include "show.h"
 
 #include <set> // Red Black Tree로 구현
+
+// 표준 문서 : set을 구현할때 tree를 사용하라고 강제하지는 않았다.
+// 동작에 대한 정의만 해놓았음. (몇초내로 나와야대~ 이런느낌으로)
+// 그래서 대부분 컴파일러는 "RED BLACK TREE"로 구현되어 있다.
 
 int main()
 {
@@ -12,16 +16,17 @@ int main()
 	s.insert(15);
 	s.insert(5);
 	s.insert(40);
+	s.insert(17);
+	s.insert(25);
 
-	auto first = s.begin();
-	auto last  = s.end();
+	auto first = s.begin(); // 가장 왼쪽
+	auto last  = s.end(); // 오른쪽 마지막 다음 노드
 
 	while (first != last)
 	{
 		std::cout << *first << std::endl;
 		++first;
 	}
-	
 }
 
 
