c445e82f6350435a1fd63981e2c2d65e0194a7af
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 11:40:25 +0900

    Update 5_유틸리티1.cpp

--- DAY3/5_유틸리티1.cpp
+++ DAY3/5_유틸리티1.cpp
@@ -16,9 +16,12 @@ int main()
 	// C++11 부터 pair의 일반화 버전이 tuple 제공
 	std::tuple<int, double, int> t1(1, 3.4, 3);
 
+	// template 인자라서 <1> 안에 변수를 못넣는다.
 	std::cout << std::get<1>(t1) << std::endl; // 3.4
 
-
+	// pair, tuple 을 만드는 함수도 있습니다.
+	auto p3 = std::make_pair (1, 3.4);
+	auto t4 = std::make_tuple (1, 3.4, 1);
 }
 
 
