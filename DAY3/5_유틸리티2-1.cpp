f4c9430660ba7118fd6100d8548d2833ebaa32f0
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 11:54:32 +0900

    Add files via upload

---
+++ DAY3/5_유틸리티2-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream> // 표준 입출력 스트림
+#include <fstream>  // 화일 스트림
+#include <sstream>  // 메모리(스트링)스트림
+#include <string>
+
+int main()
+{
+	double d = 3.1415;
+
+	// "d = 3.1415" 라는 문자열을 만들어 보라
+	// C언어
+	char s[32];
+	sprintf(s, "d = %lf", d); // buffer overflow 가 발생할 수 있다.
+	snprintf(s, 32, "d = %lf", d); // 데이터 손실 가능성이 있다.
+
+	// C++
+	std::ostringstream oss;
+	oss << "d = " << d;
+
+	std::string ss = oss.str();
+	std::cout << ss;
+}
