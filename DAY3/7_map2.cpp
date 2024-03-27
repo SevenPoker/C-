c9b4ca560158ce85170ee41020164c0479e92c9a
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 15:41:53 +0900

    Update 7_map2.cpp

--- DAY3/7_map2.cpp
+++ DAY3/7_map2.cpp
@@ -1,13 +1,40 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <sstream>
 #include <string>
 #include <list>
 #include <map>
 #include <algorithm>
+#include "show.h"
 
 int main()
 {
+    std::ifstream fin("7_map2.cpp");
+    std::string s;
+    int no = 0;
+
+    std::string word;
+    std::map<std::string, std::vector<int>> index;
+    // 자료구조를 생각해보자
+
+    while(std::getline(fin, s)) // getline을 하면 문장 전체를 읽어온다.
+    {
+        ++no;
+        std::istringstream iss(s);
+        
+        while( iss >> word)
+        {
+            index[word].push_back(no); // 없으면 만들어라
+        }
+    }
+    // =========================
+
+    for (const auto& [key, value] : index)
+    {
+        std::cout << key << " : ";
+        show(value);
+    }
+
 
 }
 
