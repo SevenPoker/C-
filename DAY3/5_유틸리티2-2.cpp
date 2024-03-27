8eb026417ba4198bb4e9a535cecef159014eb4cf
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 11:58:48 +0900

    Add files via upload

---
+++ DAY3/5_유틸리티2-2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> // 표준 입출력 스트림
+#include <fstream>  // 화일 스트림
+#include <sstream>  // 메모리(스트링)스트림
+#include <string>
+
+int main()
+{
+	std::ostringstream oss;
+	std::istringstream iss("to be or not to be");
+
+	std::string s;
+///	std::cin >> s; 표준 입력에서 한단어 입력
+
+//	iss >> s; // iss가 가진 문자열중에 한 단어만 출력할게~ (to)
+
+	while (iss >> s)
+		std::cout << s << std::endl;
+}
