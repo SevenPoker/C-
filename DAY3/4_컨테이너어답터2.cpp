9c70a7a6efaa4a5c83c6a7018c206075cc748bd2
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 10:48:46 +0900

    Update 4_컨테이너어답터2.cpp

--- DAY3/4_컨테이너어답터2.cpp
+++ DAY3/4_컨테이너어답터2.cpp
@@ -1,4 +1,4 @@
-﻿// 3개의 컨테이너 어답터가 있습니다.
+// 3개의 컨테이너 어답터가 있습니다.
 #include <stack>
 #include <queue> // queue, priority_queue
 #include "show.h"
@@ -7,6 +7,14 @@ int main()
 {
 	std::stack<int> s1;
 	s1.push(10);
+	s1.pop(); // 제거
+
+	int n = s1.top();
+
+	std::queue<int> q1;
+	q1.push(10);
+
+	int n2 = q1.front(); // <== !!
 
 
 }
