43f1a6ec85c39d2a52000a4c7ea2b8b9793024d8
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 10:09:18 +0900

    Update 3_array1-1.cpp

--- DAY3/3_array1-1.cpp
+++ DAY3/3_array1-1.cpp
@@ -1,5 +1,6 @@
-﻿#include <vector>
+#include <vector>
 #include "show.h"
+#include <array>
 
 void f1()
 {
@@ -9,6 +10,11 @@ void f1()
 void f2()
 {
 	std::vector<int> v(10);
+}
+
+void f3()
+{
+	std::array<int,10> arr;
 }
 
 void use_array()
@@ -21,8 +27,14 @@ void use_vector()
 	for(int i = 0; i < 1'000'000; i++) f2();
 }
 
+void use_std_array()
+{
+	for(int i = 0; i < 1'000'000; i++) f3();
+}
+
 int main()
 {
 	chronometry(use_array);
 	chronometry(use_vector);
+	chronometry(use_std_array);
 }
