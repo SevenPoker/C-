e53b06b4f7822eb0c8d712e875d2a113767fe6ec
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-27 14:52:08 +0900

    Update 6_set4.cpp

--- DAY3/6_set4.cpp
+++ DAY3/6_set4.cpp
@@ -1,10 +1,11 @@
-﻿#include "show.h"
+#include "show.h"
 #include <set>
 
 struct Compare
 {
 	bool operator()(int a, int b) const
 	{
+		std::cout << a << "    " << b <<std::endl;
 		return (a / 10) < (b / 10);
 	}
 };
@@ -18,12 +19,16 @@ int main()
 	s.insert(25);
 	s.insert(45);
 	s.insert(35);
-	s.insert(22); 
+	//s.insert(22);  // 실패
 	show(s);
 
 	// #2. 아래 2개의 검색 결과는 어떻게 나올까요 ?
 	// #2-1. equivalency 
-	auto ret1 = s.find(12); 
+	auto ret1 = s.find(12); // 이진검색
+							// root 와 비교해서 왼쪽 오른쪽 결정
+							// 즉, == 연산이 아닌 set에 전달한 함수 객체를 사용해서
+							// 크지도 않고, 작지도 않으면 같다고 판단한다.
+							// 따라서 15를 찾게된ㄴ다.
 	
 	if (ret1 == s.end())
 		std::cout << "실패" << std::endl;
@@ -32,7 +37,7 @@ int main()
 
 
 	// #2-2. 아래 코드가 equality
-	auto ret2 = std::find(s.begin(), s.end(), 12); 
+	auto ret2 = std::find(s.begin(), s.end(), 12); // 실패한다.
 	if (ret2 == s.end())
 		std::cout << "실패" << std::endl;
 	else 
