75d41f27052b9cf1298c97fea125f350b2a7f660
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 10:07:37 +0900

    Update 2_cpp20_5-2.cpp

--- DAY4/2_cpp20_5-2.cpp
+++ DAY4/2_cpp20_5-2.cpp
@@ -3,25 +3,32 @@
 #include <functional>
 #include <iostream>
 
+struct Point
+{
+	int x,y;
+
+	int gety() const {return y;} // 멤버함수
+};
+
+int gety(const Point& p) {return p.y; } // 일반 단항 함수
+
 int main()
 {
-	std::string s1 = "ABC";
-	std::string s2 = "XY";
+	Point p1 = {1,1};
+	Point p2 = {1,2};
 
-	auto ret1 = std::max(s1,s2); // "XY"
-	// s1, s2 를 < 연산으로 비교한다.
+	//auto ret1 = std::ranges::max(p1,p2); // error
 
+	auto ret2 = std::ragnes::max(p1,p2,{},&Point::gety); // projection으로 멤버함수 사용
 
-	// s1, s2 중 문자열의 길이가 큰 것은?
-	auto ret2 = std::max(s1.size(), s2.size()); 
-	// 이렇게 해도 되지만, 이 코드는 반환타입도 int 이다.
+	auto ret3 = std::ranges::max(p1,p2,{},&gety); // 일반 함수 사용
 
-	auto ret3 = std::ranges::max(s1, s2, std::less{}, &std::string::size);
-	// s1, s2의 멤버중 4번째 인자로 보낸 멤버를 호출한 결과를
-	// 3번째 인자로 보낸 함수(객체)로 비교해달라.
-	// 4번쨰 인자를 projectiond 이라고 부른다.
-	// s1,s2 =====> projection을 통과한 결과를 ====> 비교함수로 전달.
+	auto ret4 = std::ranges::max(p1,p2,{},&Point::y); // public 멤버 데이터 주소
 
-	auto ret4 = std::ranges::max(s1, s2, {}, &std::string::size);
-	// 이렇게 표현할수도 있다.
+	// auto ret5 = std::ranges::max(p1,p2,{},[](Point p){return p.y;});
+
+	// projection 에는
+	// 1. public 멤버 데이터의 주소
+	// 2. 인자가 없는 멤버함수 주소
+	// 3. 인자가 한개(p1, p2 라면 인자가 Point인)인 일반함수(객체), 람다표현식
 }
