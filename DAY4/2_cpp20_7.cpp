6c76d8a76163ce67fd1daf6a01c71a39f3ffa3a2
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 10:25:12 +0900

    Update 2_cpp20_7.cpp

--- DAY4/2_cpp20_7.cpp
+++ DAY4/2_cpp20_7.cpp
@@ -15,10 +15,15 @@ int main()
 
 	// #2. 단축 표기법 (syntax sugar) 도 제공한다.
 	// => 리눅스의 파이프라인을 흉내낸것
+	// v를 그 다음에 보내, 그리고 또 그다음에 보내, 그리고 또 그다음에 보내
+	// v -> filter -> take -> reverse 순으로
 
 	auto rv2 = v | std::views::filter( [](int n){return n % 2 == 0;} )
 			     | std::views::take(3) | std::views::reverse;
 
+	// 새롭게 나오고 있는 방식 - 아래처럼 사용하게 하자!!
+	// C#의 Linq 방식, Rust의 iterator 방식
+//	auto rv3 = v.filter( [](int n){return n % 2 == 0;} ).take(3).reverse();
 
 	for(auto e : rv2)
 	{
