e3da8c5f69ccc9782b25a02ec2a9567dada4be32
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 11:52:42 +0900

    Update 6_chrono2.cpp

--- DAY4/6_chrono2.cpp
+++ DAY4/6_chrono2.cpp
@@ -1,14 +1,25 @@
-﻿#include <iostream>
+#include <iostream>
 #include <chrono>
 
 // duration 정의 : 특정 단위의 갯수(tick count)를 표현하는 자료구조
+//				  값 한개와, 단위를 관리
 //				  10초 => 1초의 10개
-//					   => 1/1000초의 10000개..
+//				  10분 => 1분의 10개
 
 int main()
 {
-	// 1. duration 사용
+	// duration 사용
+	// 1. duration은 "값 한개"만 보관한다.
+	// 2. 그런데, 템플릿 인자로 단위도 전달한다.
+
+	//                          요게 몇개가 있어야    이게 되냐??
 	std::chrono::duration<int, std::ratio<1, 100>>  d1(2); // 1/100 * 2
 	std::chrono::duration<int, std::ratio<1, 1000>> d2(d1); 
 
+	std::cout << d1.count() << std::endl;
+	std::cout << d2.count() << std::endl;
+
+	std::chrono::duration<int, std::ratio<1, 50000>> d3(d1);
+
+	std::cout << d3.count() << std::endl;
 }
