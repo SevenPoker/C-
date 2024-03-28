408d8ef7de317fa0e7c4fd56797c387ce0496ae7
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 13:57:44 +0900

    Update 4_스마트포인터0.cpp

--- DAY4/4_스마트포인터0.cpp
+++ DAY4/4_스마트포인터0.cpp
@@ -10,12 +10,24 @@ public:
 int main()
 {
 	// 포인터를 직접 사용하는 경우의 문제점
-	Ptr p = new Car; 
+	Car* p1 = new Car; 
+	Car* p2 = p1;
 
 	// #1. 메모리 누수
+	delete p1;
+
+
 	// #2. dangling pointer
+	if (p2 != nullptr)
+	{
+		p2->Go(); // 버그, p2는 0번지는 아니지만
+				  // p2가 가진 주소의 객체는 이미 파괴되었다.
+	}
+
 	// #3. double delete
-	
+	// delete 한번 더 한거
+	delete p2;
+
 }
 
 
