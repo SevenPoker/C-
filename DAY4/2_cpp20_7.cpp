e2b89d869043aec26cf17f6ce4989771176b187a
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 10:21:11 +0900

    Update 2_cpp20_7.cpp

--- DAY4/2_cpp20_7.cpp
+++ DAY4/2_cpp20_7.cpp
@@ -13,7 +13,14 @@ int main()
 	std::ranges::take_view tv(v, 5);
 	std::ranges::reverse_view rv(tv);
 
-	for(auto e : rv)
+	// #2. 단축 표기법 (syntax sugar) 도 제공한다.
+	// => 리눅스의 파이프라인을 흉내낸것
+
+	auto rv2 = v | std::views::filter( [](int n){return n % 2 == 0;} )
+			     | std::views::take(3) | std::views::reverse;
+
+
+	for(auto e : rv2)
 	{
 		std::cout << e << ", ";
 	}	
