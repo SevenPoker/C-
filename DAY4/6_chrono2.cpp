992e7ef3e40f586804eecc0a170472f0ad7c7c09
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 11:55:40 +0900

    Update 6_chrono2.cpp

--- DAY4/6_chrono2.cpp
+++ DAY4/6_chrono2.cpp
@@ -12,8 +12,10 @@ int main()
 	// 1. duration은 "값 한개"만 보관한다.
 	// 2. 그런데, 템플릿 인자로 단위도 전달한다.
 
-	//                          요게 몇개가 있어야    이게 되냐??
+
 	std::chrono::duration<int, std::ratio<1, 100>>  d1(2); // 1/100 * 2
+
+	//                          요게 몇개가 있어야    이게 되냐??
 	std::chrono::duration<int, std::ratio<1, 1000>> d2(d1); 
 
 	std::cout << d1.count() << std::endl;
