f3d8bb011af9736cf6a75a51b10b78726bd33320
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 17:01:37 +0900

    Update 5_unique_ptr4.cpp

--- DAY4/5_unique_ptr4.cpp
+++ DAY4/5_unique_ptr4.cpp
@@ -1,9 +1,18 @@
-﻿#include <memory>
+#include <memory>
 #include <iostream>
 
 int main()
 {
-	std::unique_ptr<int> up1(new int);
+	std::unique_ptr<int> up(new int); // 독점
+	std::shared_ptr<int> sp(new int); // 허용
 
-	std::unique_ptr<int> up1(new int);
+	// 다음중 에러는 ?
+	std::shared_ptr<int> sp1 = up; // error
+	std::shared_ptr<int> sp2 = std::move(up); // ok
+
+	std::unique_ptr<int> up1 = sp; // error
+	std::unique_ptr<int> up2 = std::move(sp); // error
 }
+
+// shared_ptr : 자원의 공유 허용
+// unique_ptr : 자원의 공유 허용 안함 독점
