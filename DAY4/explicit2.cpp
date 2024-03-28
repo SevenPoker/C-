878b1c319db6b5d78efd161d7a4c530f05c0016d
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 17:13:25 +0900

    Add files via upload

---
+++ DAY4/explicit2.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+#include <string>
+
+void f1(std::vector<int> v) {}
+void f2(std::string s) {}
+
+int main()
+{
+	f2("hello");
+	std::string s1 = "hello";
+	std::string s2("hello");
+
+	f1(10);
+	std::vector<int> v1=10;
+	std::vector<int> v2(10);
+}
