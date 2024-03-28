f56e1ab81e110bd7eceae5d21e58f9910f43b12f
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 17:05:04 +0900

    Add files via upload

---
+++ DAY4/explicit.cpp
@@ -0,0 +1,25 @@
+﻿#include <memory>
+#include <iostream>
+
+int main()
+{
+	std::unique_ptr<int> up(new int);
+	std::shared_ptr<int> sp(new int);
+
+	// 다음중 에러는 ?
+	std::shared_ptr<int> sp1 = up;
+	std::shared_ptr<int> sp2 = std::move(up);
+
+	std::unique_ptr<int> up1 = sp;
+	std::unique_ptr<int> up2 = std::move(sp); 
+}
+
+int* Alloc()
+{
+	return new int;
+}
+
+int main()
+{
+	Alloc();
+}
