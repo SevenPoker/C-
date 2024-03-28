d1bb516ee6c68c7959bef84a0ffb3e364cb58cf2
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 08:54:39 +0900

    Update 1_string4.cpp

--- DAY5/1_string4.cpp
+++ DAY5/1_string4.cpp
@@ -2,10 +2,19 @@
 #include <string>
 #include "show.h"
 
+using namespace std::literals;
+
 void foo(const char* s) { std::cout << "const char*\n";} // 1
 void foo(const std::string& s) { std::cout << "string";} // 2
 
 int main()
 {
-	foo("hello"); // ?
+	foo("hello"); // hello의 정확한 타입은 const char배열이다.
+				  // => const char[] => const char* 로 암시적 변환 가능
+	
+	foo("hello"s); // 2번이 불린다.
+
+
+	auto s1 = "hello"; // s1 : const char*
+	auto s2 = "hello"s; // s2 : std::string
 }
