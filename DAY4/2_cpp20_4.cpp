3cf0dcb9bcfb627ed242cd38fd585a17eb14d82d
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 09:46:55 +0900

    Update 2_cpp20_4.cpp

--- DAY4/2_cpp20_4.cpp
+++ DAY4/2_cpp20_4.cpp
@@ -13,6 +13,18 @@ T1 myfind(T1 first, T1 last, const T2& c)
 	return first; 
 }
 
+
+template<typename T1, typename S, typename T2>
+T1 cpp20find(T1 first, S last, const T2& c)
+{
+	while ( first != last && *first != c)
+		++first;
+
+	return first; 
+}
+
+
+
 int main()
 {
 	std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
@@ -20,6 +32,13 @@ int main()
 	std::counted_iterator ci(v.begin(), 5);
 
 	// ? 채워 보세요.
-	auto ret = std::find(ci, ?, 3);
+	// 아래처럼 하면 되지않을까!??
+	// ㄴㄴ 안된다 ci와 sentinel이 같은 type이어야 한다.
+	// 그래서 위의 cpp20find와 같이 만들었다.
+//	auto ret = std::find(ci, std::default_sentinel, 3);
+
+	// 기존code도 동일하게 동작을 해야하기 때문에
+	// 아래 range에 넣어놨다.
+	auto ret = std::ranges::find(ci, std::default_sentinel, 3);
 }
 
