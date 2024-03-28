f730b877c89ea98568cf53186b3f3eafff3cb369
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 09:03:12 +0900

    Add files via upload

---
+++ DAY4/1_사용자정의타입7.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+// struct hash가 template라고 알려주는 거다.
+template<typename T>
+struct Hash;
+
+
+// template specialization (특수화) 문법
+template<>
+struct Hash<int>
+{
+	int operator()(int n) const
+	{
+		std::cout << "hash for int\n"; 
+		return 0;
+	}
+}
+
+template<>
+struct Hash<double>
+{
+	int operator()(double n) const
+	{
+		std::cout << "hash for double\n"; 
+		return 0;
+	}
+}
+
+
+int main()
+{
+	Hash<int> hi;
+	Hash<double> hd;
+	int n = h(10);
+}
---
+++ DAY4/1_사용자정의타입8.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include "show.h"
+
+struct People
+{
+	std::string name;
+	int  age;
+
+	People(const std::string& n, int a) : name(n), age(a) {}
+};
+
+// C++ 표준의 std::hash 템플릿을 사용자가 만든 타입에 대해서
+// 특수화 버전을 제공하는 기법도 가능하다.
+// 아래처럼 std hash에 직접 넣을 수 도있다.
+template<>
+struct std::hash<People>
+{
+	int operator()(const People& p) const
+	{
+		std::hash<int> hi;
+		std::hash<std::string> hs;
+
+		return hs(p.name) + hi(p.age);
+	}
+}
+
+template<>
+struct std::equal_to<People>
+{
+	bool operator()(const People& p1, const People& p2) const
+	{
+		return p1.name == p2.name && p1.age == p2.age;
+	}
+}
+
+int main()
+{
+	std::unordered_set<People> s;
+
+	std::unordered_set<People,
+					   std::hash<People>, // 이 부분은 없다. (구현 필요)
+					   std::equal_to<People>, // 이부분도 없다. (구현 필요)
+					   std::allocator<People>>
+
+
+
+	s.emplace("kim", 20); // s.insert( People("kim",20))
+	s.emplace("lee", 25);
+	s.emplace("park", 30);
+
+	People peo("lee", 25);
+	auto p = s.find( peo );
+
+	std::cout << (*p).name << std::endl;
+}
+
