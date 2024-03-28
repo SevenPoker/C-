c0457f19cccb197795402b5d5611c4856fbe9498
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 11:58:07 +0900

    Add files via upload

---
+++ DAY4/6_chrono2-1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <chrono>
+
+int main()
+{
+	// 길이를 나타내는 단위를 설계해 보자.
+	using Meter = std::chrono::duration<int, std::ratio<1,1>>;
+	using KiloMeter = std::chrono::duration<int, std::ratio<1000,1>>;
+	using CentiMeter = std::chrono::duration<int, std::ratio<1,100>>;
+
+	KiloMeter km(1); // 1km
+
+	CentiMeter cm(km);
+
+	std::cout<< cm.count() <<std::endl;
+
+}
