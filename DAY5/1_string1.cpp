8e1edff00ecc828e5a81669a2bb9b0cf4b20c64b
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 08:36:39 +0900

    Update 1_string1.cpp

--- DAY5/1_string1.cpp
+++ DAY5/1_string1.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include "show.h"
+
+// STL : Standart "Template" Library
+
 /*
 template< typename CharT,
     	  typename Traits = std::char_traits<CharT>,
@@ -9,9 +12,11 @@ class basic_string
 {
 };
 using string = basic_string<char>;
+using wstring = basic_string<wchar_t>;
 */
 
 int main()
 {
-	std::string s = "hello";
+	std::string s = "hello";		// ansi 문자열
+	std::wstring ws =L"hello";		// unicode 문자열
 }
