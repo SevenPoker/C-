9a527c4e47508d4c328641883cb8d231e78e97d0
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 17:15:20 +0900

    Update explicit2.cpp

--- DAY4/explicit2.cpp
+++ DAY4/explicit2.cpp
@@ -1,4 +1,4 @@
-﻿#include <vector>
+#include <vector>
 #include <string>
 
 void f1(std::vector<int> v) {}
@@ -6,11 +6,15 @@ void f2(std::string s) {}
 
 int main()
 {
-	f2("hello");
-	std::string s1 = "hello";
-	std::string s2("hello");
+	// 안이상하다
+	f2("hello"); // ok
+	std::string s1 = "hello"; // ok
+	std::string s2("hello"); // ok
+							// 그래서 string(const char*) 생성자는 explicit 아님.
 
-	f1(10);
-	std::vector<int> v1=10;
-	std::vector<int> v2(10);
+
+	f1(10); // error (모양이 이상핟.)
+	std::vector<int> v1=10; // error 
+	std::vector<int> v2(10); // ok
+						// vector(int) 생성자는 explicit!!
 }
