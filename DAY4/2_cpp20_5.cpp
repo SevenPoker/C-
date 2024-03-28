94d84abf29266d02e8fa1d26ba15c2de186f6f10
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 09:49:32 +0900

    Update 2_cpp20_5.cpp

--- DAY4/2_cpp20_5.cpp
+++ DAY4/2_cpp20_5.cpp
@@ -8,10 +8,15 @@
 int main()
 {
 	std::list<int> s1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	std::list<int> s2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	std::list<int> s2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 	// s1 의 모든 내용을 s2로 복사해 보세요
-	
+	std::copy(s1.begin(), s1.end(), s2.begin());
+
+	// C++20
+	// => 구간이 요구되는 인자는 "컨테이너 이름"으로 전달 가능합니다.
+	std::ranges::copy(s1.begin(), s1.end(), s2.begin());
+	std::ranges::copy(s1,s2.begin()); // ok
 	
 	show(s2);
 }
