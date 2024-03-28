343ce25b27874c653d588eba5ec3576a2704623e
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 08:51:27 +0900

    Update 1_string3.cpp

--- DAY5/1_string3.cpp
+++ DAY5/1_string3.cpp
@@ -2,6 +2,19 @@
 #include <string>
 #include "show.h"
 
+// STL string 은 "SSO" 알고리즘 사용
+// => Small String Optimization
+// string 만들어서 문자열을 만들면 heap에 올려서
+// 포인터로 가리키는데
+// 문자열이 작으면 포인터로 굳이 가리켜야되나?? 그래서
+// 포인터 자리에 스트링을 저장한다.
+
+// 지금은 다 그렇게 되어있다.
+
+
+
+// 16자 이하 : string 객체 내부에 문자열 보관
+// 16자 이상 : heap을 할당해서 문자열 보관
 using dstring = std::basic_string<char, std::char_traits<char>, debug_alloc<char>>;
 
 int main()
