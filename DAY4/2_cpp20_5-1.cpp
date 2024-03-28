5daa533a07a280567a3ea9efdfe3aaa177727a66
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 09:58:40 +0900

    Update 2_cpp20_5-1.cpp

--- DAY4/2_cpp20_5-1.cpp
+++ DAY4/2_cpp20_5-1.cpp
@@ -1,23 +1,27 @@
-#include <vector>
-#include <list>
+#include <string> 
 #include <algorithm>
+#include <functional>
 #include <iostream>
-#include <ranges>
-#include "show.h"
 
 int main()
 {
-	std::list<int> s1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-	std::list<int> s2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	std::string s1 = "ABC";
+	std::string s2 = "XY";
 
-	// s1 의 모든 내용을 s2로 복사해 보세요
-	std::copy(s1.begin(), s1.end(), s2.begin());
+	auto ret1 = std::max(s1,s2); // "XY"
+	// s1, s2 를 < 연산으로 비교한다.
 
-	// C++20
-	// => 구간이 요구되는 인자는 "컨테이너 이름"으로 전달 가능합니다.
-	std::ranges::copy(s1.begin(), s1.end(), s2.begin());
-	std::ranges::copy(s1,s2.begin()); // ok
-	
-	show(s2);
+
+	// s1, s2 중 문자열의 길이가 큰 것은?
+	auto ret2 = std::max(s1.size(), s2.size()); 
+	// 이렇게 해도 되지만, 이 코드는 반환타입도 int 이다.
+
+	auto ret3 = std::ranges::max(s1, s2, std::less{}, &std::string::size);
+	// s1, s2의 멤버중 4번째 인자로 보낸 멤버를 호출한 결과를
+	// 3번째 인자로 보낸 함수(객체)로 비교해달라.
+	// 4번쨰 인자를 projectiond 이라고 부른다.
+	// s1,s2 =====> projection을 통과한 결과를 ====> 비교함수로 전달.
+
+	auto ret4 = std::ranges::max(s1, s2, {}, &std::string::size);
+	// 이렇게 표현할수도 있다.
 }
-
