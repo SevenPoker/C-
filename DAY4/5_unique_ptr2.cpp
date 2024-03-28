1e08d84afc97aa96fece10361f5e4c326bcd3b44
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 16:44:15 +0900

    Update 5_unique_ptr2.cpp

--- DAY4/5_unique_ptr2.cpp
+++ DAY4/5_unique_ptr2.cpp
@@ -1,8 +1,20 @@
-﻿#include <memory>
+#include <memory>
 #include <iostream>
+
+// std::shared_ptr : 자원을 공유할 수 있는 스마트 포인터
+// std::weak_ptr   : 소유권이 없는 스마트 포인터
+// std::unique_ptr : 자원을 독점하는 스마트 포인터
 
 int main()
 {
-	std::unique_ptr<int> up1(new int);
-	std::unique_ptr<int> up2 = up1;
+	std::unique_ptr<int> up1(new int); // ok
+//	std::unique_ptr<int> up2 = up1; // error
+	// 다른 smart pointer와 공유하는 것은 오류!!
+
+	std::unique_ptr<int> up3 = std::move(up1); // ok
+
+	std::unique_ptr<int> up4 = std::make_unique<int>();
+
+	std::cout << sizeof(up1) << std::endl;
+	// overhead 가 없다.
 }
