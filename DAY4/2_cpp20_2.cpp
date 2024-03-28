750aa8986eda73ff577fde80c064dbf82ab64467
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-28 09:40:37 +0900

    Update 2_cpp20_2.cpp

--- DAY4/2_cpp20_2.cpp
+++ DAY4/2_cpp20_2.cpp
@@ -3,6 +3,17 @@
 #include <iostream>
 #include <ranges>
 
+// 핵심 2
+// 1. 기존 알고리즘은 구간을 전달하기 위헤 "반복자 2개"를 인자로 받고 있습니다.
+// 2. 그런데, C++20 부터 일부 반복자는 한개로 구간 표현.
+//    => last를 표현할ㄹ 방법이 필요했다.
+//    => "sentonel(보초병)" 의 개념이 등장한다.
+
+// C++98 구간 : first ~ last
+// C++20 구간 : first ~ sentinel
+//						=> last 일 수도 있고, default_sentinel 일 수도있다.
+
+
 int main()
 {
 	std::vector<int> v = {1,2,3,4,5,6,7,8,9,10};
@@ -10,10 +21,11 @@ int main()
 	std::counted_iterator ci(v.begin(), 5);
 
 	// ? 를 생각해 보세요
-	auto ret = std::find(ci, ?, 3);
+//	auto ret = std::find(ci, ?, 3);
 
 
-	while( ci.count() != 0 )
+//	while( ci.count() != 0 )
+	while( ci != std::default_sentinel )
 	{
 		std::cout << *ci << std::endl;
 		++ci;
