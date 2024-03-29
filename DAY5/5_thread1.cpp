2fbbf1b5809e0b203da67771d1d0166721ed4a3f
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 11:44:30 +0900

    Update 5_thread1.cpp

--- DAY5/5_thread1.cpp
+++ DAY5/5_thread1.cpp
@@ -1,15 +1,17 @@
-﻿#include <iostream>
-#include <thread> 
+#include <iostream>
+#include <thread> // thread 관련 헤더. 
 #include <chrono>
 using namespace std::literals;
 
-// this_thread namespace 
+// std::this_thread namespace 
+// => thread 관련 몇가지 일반 함수 제공
+
 
 int main()
 {
-    std::cout << std::this_thread::get_id() << std::endl;
+    std::cout << std::this_thread::get_id() << std::endl; // 현재 스레드 ID 얻기
 
-    std::this_thread::sleep_for(3s);		
+    std::this_thread::sleep_for(3s); // 현재 thread를 재울때
 
 }
 
