5c764a2b953c06b1c865d2de59d2b805d4bd0ead
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 11:10:40 +0900

    Update 4_traits4.cpp

--- DAY5/4_traits4.cpp
+++ DAY5/4_traits4.cpp
@@ -1,5 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
 #include <type_traits>
+
+// type traits 종류
+// 1. 타입 질의     : std::is_pointer<T>::value        ===> return bool;
+// 2. 변형 타입얻기 : std::remove_pointer<T>::type   (포인터를 제거한 타입을 얻고 싶을때) 
+//												===> T가 int* 였다면 return int;
 
 template<typename T> void foo(const T& b)
 {
