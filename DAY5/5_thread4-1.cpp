0ebcdde0ed5b2699d0f0174c354358fc3c3d6e01
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 13:36:38 +0900

    Add files via upload

---
+++ DAY5/5_thread4-1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <chrono>
+using namespace std::literals;
+
+// 방법 #1. out parameter 사용
+
+void add(int a, int b, int& s)
+{
+	s = a + b;
+}
+
+int main()
+{
+	int ret = 0;
+	std::thread t(&add, 1, 2, std::ref(ret));
+
+	t.join();
+
+	std::cout << ret << std::endl;
+}
