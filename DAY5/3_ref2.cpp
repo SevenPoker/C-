b6e32746410777b66ff42a13d298b0a94e30c390
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 10:00:47 +0900

    Update 3_ref2.cpp

--- DAY5/3_ref2.cpp
+++ DAY5/3_ref2.cpp
@@ -1,1 +1,37 @@
-﻿// 1번 복사해 오세요
+#include <iostream>
+#include <functional>
+
+// reference_wrapper 원리는?
+// reference의 기계어 코드는 다 포인터다.
+
+template<typename T>
+class reference_wrapper
+{
+    T* obj;
+public:
+    reference_wrapper(T& r) : obj(&r) {}
+
+    // 변환 연산자 : 객체가 다른타입으로 변환이 필요 할 때 호출되는 함수
+    operator T&() {return obj;}
+};
+
+int main()
+{
+	int n1 = 10;
+	int n2 = 20;
+
+    reference_wrapper<int> r1 = n1;
+	reference_wrapper<int> r2 = n2;
+
+	r1 = r2;
+
+    int& r3 = r1; // r1.operator int&() 가 있으면 된다.
+                  // 변환 연산자 함수
+
+
+	std::cout << n1 << std::endl;
+	std::cout << n2 << std::endl;	
+	std::cout << r1 << std::endl;
+	std::cout << r2 << std::endl;
+}
+
