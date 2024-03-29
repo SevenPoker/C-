f8178b612a3063a5a03c555b73c9e5be8fb72de1
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 14:17:39 +0900

    Add files via upload

---
+++ DAY5/5_thread6-1.cpp
@@ -0,0 +1,18 @@
+﻿#include <atomic>
+#include <iostream>
+
+struct Point
+{
+	int x;
+	int y;
+
+
+};
+
+int main()
+{
+	std::atomic<Point> p1;
+	std::atomic<Point> p2(p1.lock()); // 멀티스레드 안전하게 복사
+
+	std::cout << p1.lock_free() <<std::endl;
+}
