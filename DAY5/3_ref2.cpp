ce0f32c0c237f84d0fa434bc2ca945779200f0a2
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 10:03:43 +0900

    Update 3_ref2.cpp

--- DAY5/3_ref2.cpp
+++ DAY5/3_ref2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <functional>
 
+// 결국 reference_wrapper는
+// 1.객체(변수)의 주소를 보관하고 있다가
+// 2."raw reference" 로 암시적 변환이 가능한 도구이다.
 // reference_wrapper 원리는?
 // reference의 기계어 코드는 다 포인터다.
 
@@ -12,7 +15,7 @@ public:
     reference_wrapper(T& r) : obj(&r) {}
 
     // 변환 연산자 : 객체가 다른타입으로 변환이 필요 할 때 호출되는 함수
-    operator T&() {return obj;}
+    operator T&() {return *obj;}
 };
 
 int main()
@@ -31,7 +34,7 @@ int main()
 
 	std::cout << n1 << std::endl;
 	std::cout << n2 << std::endl;	
-	std::cout << r1 << std::endl;
+	std::cout << r1 << std::endl;  // std::cout << r1.operator int&() 로 컴파일러가 변환해준다.
 	std::cout << r2 << std::endl;
 }
 
