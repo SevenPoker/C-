80943b47720be6643ee53b462e22f20d04171f83
Pythagoras_Lee <40813748+SevenPoker@users.noreply.github.com>
2024-03-29 12:05:27 +0900

    Add files via upload

---
+++ DAY5/5_thread3-1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex m;
+int shared_data = 10;
+
+void foo()
+{
+	int data = 0;
+
+	m.lock();
+	shared_data = 200;
+	m.unlock();
+
+}
+
+int main()
+{
+	std::thread t1(&foo);
+	std::thread t2(&foo);
+
+	t1.join();
+	t2.join();
+}
+
